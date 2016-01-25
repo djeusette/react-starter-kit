d7b4dc2e2d80b4ad8fc25437908260c6777120ae
David Jeusette <jeusette.david@gmail.com>
2016-01-25 23:23:19 +0100

    Revert NoFoundPage changes

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -27,7 +27,7 @@ class NotFoundPage extends Component {
 
   render() {
     return (
-      <div className={s.container}>
+      <div>
         <h1>{title}</h1>
         <p>Sorry, but the page you were trying to view does not exist.</p>
       </div>
